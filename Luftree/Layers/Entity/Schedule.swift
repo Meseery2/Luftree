89976892b335340524589e92ba7140d65491a21b
Meseery <34045596+Meseery2@users.noreply.github.com>
2020-03-24 00:58:30 +0200

    Remove Header.

--- Luftree/Layers/Entity/Schedule.swift
+++ Luftree/Layers/Entity/Schedule.swift
@@ -1,11 +1,3 @@
-//
-//  Schedule.swift
-//  Luftree
-//
-//  Created by Mohamed EL Meseery on 3/11/19.
-//  Copyright © 2019 Meseery. All rights reserved.
-//
-
 import Foundation
 
 struct Schedule: Codable {
