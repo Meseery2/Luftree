1f965f80e5b34900f52bbb96fb5fa4bf504c0edb
Meseery <34045596+Meseery2@users.noreply.github.com>
2020-03-24 00:50:03 +0200

    Remove Another Header.

--- Luftree/Layers/Entity/LFToken.swift
+++ Luftree/Layers/Entity/LFToken.swift
@@ -1,11 +1,3 @@
-//
-//  LFToken.swift
-//  Luftree
-//
-//  Created by Mohamed EL Meseery on 3/11/19.
-//  Copyright © 2019 Meseery. All rights reserved.
-//
-
 import Foundation
 
 struct Token: Codable {
